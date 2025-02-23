88b35adaab7e25a096fa8e3e71e3b9185feba3c1
lomoru <lorenzomorenoruiz@gmail.com>
2025-02-23 20:52:46 +0100

    Primer commit

---
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
@@ -0,0 +1,93 @@
+package dam.pmdm.spyrothedragon;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import dam.pmdm.spyrothedragon.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    NavController navController = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        if (navHostFragment != null) {
+            navController = NavHostFragment.findNavController(navHostFragment);
+            NavigationUI.setupWithNavController(binding.navView, navController);
+            NavigationUI.setupActionBarWithNavController(this, navController);
+        }
+
+        binding.navView.setOnItemSelectedListener(this::selectedBottomMenu);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.navigation_characters ||
+                    destination.getId() == R.id.navigation_worlds ||
+                    destination.getId() == R.id.navigation_collectibles) {
+                // Para las pantallas de los tabs, no queremos que aparezca la flecha de atrás
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            }
+            else {
+                // Si se navega a una pantalla donde se desea mostrar la flecha de atrás, habilítala
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        });
+
+    }
+
+    private boolean selectedBottomMenu(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.nav_characters)
+            navController.navigate(R.id.navigation_characters);
+        else
+        if (menuItem.getItemId() == R.id.nav_worlds)
+            navController.navigate(R.id.navigation_worlds);
+        else
+            navController.navigate(R.id.navigation_collectibles);
+        return true;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla el menú
+        getMenuInflater().inflate(R.menu.about_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Gestiona el clic en el ítem de información
+        if (item.getItemId() == R.id.action_info) {
+            showInfoDialog();  // Muestra el diálogo
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showInfoDialog() {
+        // Crear un diálogo de información
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.title_about)
+                .setMessage(R.string.text_about)
+                .setPositiveButton(R.string.accept, null)
+                .show();
+    }
+
+
+
+}
